e44b0ceb3e941a3390e2bdc00202dc4ea5d3c826
LZH <1604807798@qq.com>
2018-11-24 17:36:47 +0800

    今天写了login静态页面和studentInfo静态页面，要下课了，暂时就先这里吧，哈哈哈

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '学生信息管理系统' });
 });
 
 module.exports = router;
